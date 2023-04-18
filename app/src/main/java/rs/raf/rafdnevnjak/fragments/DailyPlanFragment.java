2749402fe857aa63da699b3a3453aaff4b1d11ba
cvele0 <vladancvjetkovic@gmail.com>
2023-04-18 12:50:26 +0200

    daily plan view done

--- app/src/main/java/rs/raf/rafdnevnjak/fragments/DailyPlanFragment.java
+++ app/src/main/java/rs/raf/rafdnevnjak/fragments/DailyPlanFragment.java
@@ -2,14 +2,22 @@ package rs.raf.rafdnevnjak.fragments;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import rs.raf.rafdnevnjak.R;
 
 public class DailyPlanFragment extends Fragment {
+    private TextView dateText;
+    private CheckBox checkBox;
+    private SearchView searchView;
+    private RecyclerView recyclerView;
     public DailyPlanFragment() {
         super(R.layout.fragment_daily_plan);
     }
@@ -31,7 +39,10 @@ public class DailyPlanFragment extends Fragment {
     }
 
     private void initView(View view) {
-
+        dateText = view.findViewById(R.id.dailyPlanDateText);
+        checkBox = view.findViewById(R.id.dailyPlanCheckBox);
+        searchView = view.findViewById(R.id.dailyPlanSearchView);
+        recyclerView = view.findViewById(R.id.dailyPlanRecyclerView);
     }
     private void initListeners() {
 
